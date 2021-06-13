7d7e9f9d547469ffecfb360e5f5a1b8974a36d34
root <mohadese.mousavisaeed@gmail.com>
2021-06-13 17:55:58 +0430

    homepage structure

---
+++ crwn-clothing/src/homepage.component.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './homepage.style.scss'
+
+export const Homepage = (params) => {
+    return(
+        <div className={'homepage'}>
+            <div className="directory-menu">
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtite">Subtitle</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtite">Subtitle</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtite">Subtitle</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtite">Subtitle</span>
+                    </div>
+                </div>
+
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title</h1>
+                        <span className="subtite">Subtitle</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
